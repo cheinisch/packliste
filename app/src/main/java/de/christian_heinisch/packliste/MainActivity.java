6c67fffaa426fa6324d0a47bec9154cb5fa2f3af
cheinisch <chris@christian-heinisch.de>
2017-01-01 14:18:22 +0100

    first commit

--- app/src/main/java/de/christian_heinisch/packliste/MainActivity.java
+++ app/src/main/java/de/christian_heinisch/packliste/MainActivity.java
@@ -83,11 +83,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_newtrip){
 
-        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_aboutapp){
 
